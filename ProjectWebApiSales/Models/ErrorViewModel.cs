7b39ea2d09535c73a054bea90e40167c848d8622
William Henrique Cirino <williamhcirino@brq.com>
2019-12-10 17:54:56 -0300

    Add project files.

---
+++ ProjectWebApiSales/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectWebApiSales.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

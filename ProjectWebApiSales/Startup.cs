8a08038fea8a2298c5d93a183f8bb5adcc90ebee
William Henrique Cirino <williamhcirino@brq.com>
2019-12-19 17:31:20 -0300

    MySql adaptation and first migration

--- ProjectWebApiSales/Startup.cs
+++ ProjectWebApiSales/Startup.cs
@@ -37,7 +37,8 @@ namespace ProjectWebApiSales
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<ProjectWebApiSalesContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("ProjectWebApiSalesContext")));
+                    options.UseMySql(Configuration.GetConnectionString("ProjectWebApiSalesContext"), builder =>
+                      builder.MigrationsAssembly("ProjectWebApiSales")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
